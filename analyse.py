9f0f139398aa37b38b5e4e982a949487f923c599
longjiawei <429651071@qq.com>
2018-05-23 17:09:00 +0800

    clip(0,20)
    #3#

--- analyse.py
+++ analyse.py
@@ -7,28 +7,30 @@ from sklearn import metrics
 
 test_data = pd.read_csv('./data/test.csv')
 sales_data = pd.read_csv('./data/sales_train.csv')
-# sales_data = sales_data[sales_data['date_block_num'] < 3]
+sales_data['item_cnt_day'] = sales_data['item_cnt_day'].clip(0,20)
 sales_data = sales_data.groupby(['date_block_num','shop_id','item_id'],as_index=False).sum()
+sales_data['item_cnt_day'] = sales_data['item_cnt_day'].clip(0,20)
 sales_data = sales_data.drop(['date_block_num','item_price'],axis = 1)
 x = sales_data.iloc[:, 0:2].values
 y = sales_data.iloc[:, 2:3].values
-# print(sales_data)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0, random_state = 0)
 # model = LinearRegression()
 
 from sklearn import ensemble
-print('-'*25 + 'start training' + '-' * 25)
+print('-' * 33 + 'start training' + '-' * 33)
+# from sklearn import svm
+# model = svm.SVR()
 model = ensemble.GradientBoostingRegressor(n_estimators=100)#这里使用100个决策树
 model.fit(x_train, y_train)
-print('-'*25 + 'finish training' + '-' * 25)
+print('-'*32 + 'finish training' + '-' * 32)
 # y_pred = model.predict(x_test)
 # # 用scikit-learn计算MSE
 # print("MSE:",metrics.mean_squared_error(y_test, y_pred))
 # # 用scikit-learn计算RMSE
 # print("RMSE:",np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-print('-' * 25 + 'start reading data' + '-' * 25)
+print('-' * 31 + 'start reading data' + '-' * 31)
 save_path = './data/submission.csv'
-cnt = 0
+
 d = {'ID': [], 'item_cnt_month': []}
 x_final_test = []
 for indexs in test_data.index:
@@ -36,19 +38,11 @@ for indexs in test_data.index:
     shop_id = test_data.loc[indexs].values[1]
     item_id = test_data.loc[indexs].values[2]
     x_final_test.append([shop_id, item_id])
-    # result_list = sales_data[(sales_data['shop_id'] == shop_id) & (sales_data['item_id'] == item_id)]['item_cnt_day'].tolist()
-    # if (len(result_list) == 0):
-    #     d['item_cnt_month'].append(0.5)
-    #     cnt += 1
-    #     continue
-    # else:
-    #     d['item_cnt_month'].append(result_list[0])
-    #     cnt += 1
-print('-' * 25 + 'end reading data' + '-' * 25)
-print('-' * 25 + 'start predicting' + '-' * 25)
+print('-' * 32 + 'end reading data' + '-' * 32)
+print('-' * 33 + 'start predicting' + '-' * 33)
 y_pred = model.predict(x_final_test)
-print('-' * 25 + 'end predicting' + '-' * 25)
-d['item_cnt_month'] = y_pred.round(0)
+print('-' * 33 + 'end predicting' + '-' * 33)
+d['item_cnt_month'] = y_pred / 20.0
 result = pd.DataFrame(d, columns=['ID', 'item_cnt_month'])
 result.to_csv(save_path,index=0)
 # print(sales_data)
